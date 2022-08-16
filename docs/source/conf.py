47c8c216e6176d812e863876bc037d34a5a5e9dc
Manon Edde <60670074+Manonedde@users.noreply.github.com>
2022-08-16 18:43:04 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'The high-frequency MRI database'
+copyright = '2022, Edde'
+author = 'Edde'
 
 release = '0.1'
 version = '0.1.0'
